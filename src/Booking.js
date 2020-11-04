b4216ea71aa8ffd81f178c705636fd130d768bfb
Kara Caputo <kncaputo@gmail.com>
2020-11-04 15:10:31 -0500

    Add name property to booking

--- src/Booking.js
+++ src/Booking.js
@@ -1,6 +1,7 @@
 export default class Booking {
-  constructor(rawData) {
+  constructor(rawData, name) {
     this.id = rawData.id;
+    this.name = name || null;
     this.userID = rawData.userID;
     this.date = rawData.date;
     this.roomNumber = rawData.roomNumber;
--- test/booking-test.js
+++ test/booking-test.js
@@ -1,13 +1,15 @@
 import chai from 'chai';
 const expect = chai.expect;
-import Booking from '../src/Booking'
+import Booking from '../src/Booking';
 
 describe('Booking', () => {
   let booking;
+  let booking2;
   let bookingData = {"id":"1abcde2f3h11ij4lm","userID":1,"date":"2020/01/01","roomNumber":11,"roomServiceCharges":[]};
 
   beforeEach(() => {
-    booking = new Booking(bookingData);
+    booking = new Booking(bookingData, 'Carlton');
+    booking2 = new Booking(bookingData);
   });
 
   describe('Constructor', () => {
@@ -17,6 +19,14 @@ describe('Booking', () => {
 
     it('should have an id', () => {
       expect(booking.id).to.deep.equal('1abcde2f3h11ij4lm');
+    });
+
+    it('should have an name', () => {
+      expect(booking.name).to.deep.equal('Carlton');
+    });
+
+    it('should default to null when a name isn\'t provided', () => {
+      expect(booking2.name).to.deep.equal(null);
     });
 
     it('should have a user id', () => {
