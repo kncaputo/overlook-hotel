d4134d1fb8f96099a466eaef35209344acc7511d
Kara Caputo <kncaputo@gmail.com>
2020-11-04 19:15:33 -0500

    Add methods for find user name find user id

--- src/HotelOperation.js
+++ src/HotelOperation.js
@@ -3,22 +3,28 @@ import Booking from '../src/Booking';
 import User from '../src/User';
 
 export default class HotelOperation {
-  constructor(rawRoomsData) {
+  constructor() {
     this.roomsRecord = [];
     this.bookingsRecord = [];
-    this.users = [];
+    this.usersRecord = [];
   }
 
-  createRoom(rawRoomData) {
-    let room = new Room(rawRoomData);
+  createRoom(rawRoom) {
+    let room = new Room(rawRoom);
     this.roomsRecord.push(room);
     return room;
   }
 
-  createBooking(rawBookingData) {
-    let booking = new Booking(rawBookingData);
+  createBooking(rawBooking) {
+    let booking = new Booking(rawBooking);
     this.bookingsRecord.push(booking);
     return booking;
+  }
+
+  createUser(rawUser) {
+    let user = new User(rawUser);
+    this.usersRecord.push(user);
+    return user;
   }
 
   createRoomsRecord(rawRoomsData) {
@@ -30,6 +36,32 @@ export default class HotelOperation {
   createBookingsRecord(rawBookingsData) {
     let bookingsRecord = rawBookingsData.forEach(rawBooking => {
       this.createBooking(rawBooking);
+    })
+  }
+
+  createUsersRecord(rawUsersData) {
+    let users = rawUsersData.forEach(rawUser => {
+      this.createUser(rawUser);
+    })
+  }
+
+  findUserName(userID) {
+    let searchedUser = this.usersRecord.find(user => {
+      return user.id === userID;
+    });
+    return searchedUser.name;
+  }
+
+  findUserID(name) {
+    let searchedUser = this.usersRecord.find(user => {
+      return user.name === name;
+    });
+    return searchedUser.id;
+  }
+
+  filterBookingsByName(name) {
+    this.bookings.filter(booking => {
+      booking.
     })
   }
 }
--- test/hotel-operation-test.js
+++ test/hotel-operation-test.js
@@ -11,6 +11,7 @@ describe('HotelOperation', () => {
   let room;
   let bookingsData;
   let roomsData;
+  let usersData;
 
   describe('Constructor', () => {
     beforeEach(() => {
@@ -30,22 +31,23 @@ describe('HotelOperation', () => {
     });
 
     it('should start with an empty users record', () => {
-      expect(hotelOperation.users).to.deep.equal([]);
+      expect(hotelOperation.usersRecord).to.deep.equal([]);
     });
   });
 
-  describe('Methods', () => {
+  describe.only('Methods', () => {
     beforeEach(() => {
       hotelOperation = new HotelOperation();
-      bookingsData = [{"id":"1abcde2f3h11ij4lm","userID":1,"date":"2020/01/01","roomNumber":1,"roomServiceCharges":[]},{"id":"5fwrgu4i7k55hl6t6","userID":13,"date":"2020/01/10","roomNumber":12,"roomServiceCharges":[]}];
+      bookingsData = [{"id":"1abcde2f3h11ij4lm","userID":1,"date":"2020/01/01","roomNumber":1,"roomServiceCharges":[]},{"id":"5fwrgu4i7k55hl6t6","userID":13,"date":"2020/01/10","roomNumber":12,"roomServiceCharges":[]},{"id":"5fwrgu4i7k55hl6t6","userID":1,"date":"2020/01/02","roomNumber":6,"roomServiceCharges":[]}];
       roomsData = [{"number":1,"roomType":"residential suite","bidet":true,"bedSize":"queen","numBeds":1,"costPerNight":123.4},{"number":2,"roomType":"suite","bidet":false,"bedSize":"full","numBeds":2,"costPerNight":477.38},{"number":3,"roomType":"single room","bidet":false,"bedSize":"king","numBeds":1,"costPerNight":491.14}];
+      usersData = [{"id":1,"name":"Anson Aimes"},{"id":2,"name":"Theo Hernandez"},{"id":3,"name":"Jane Schmoe"}];
     });
 
     it('should have a method to create instances of room when passed an array', () => {
       expect(hotelOperation.createRoom(roomsData[0])).to.be.an.instanceof(Room);
     });
 
-    it('should push new booking instance to the bookings record', () => {
+    it('should push new room instance to the rooms record', () => {
       expect(hotelOperation.roomsRecord.length).to.deep.equal(0);
 
       hotelOperation.createRoom(roomsData[0]);
@@ -53,9 +55,8 @@ describe('HotelOperation', () => {
       expect(hotelOperation.roomsRecord.length).to.deep.equal(1);
     });
 
-
     it('should have a method to create instances of booking when passed an array', () => {
-      expect(hotelOperation.createBooking(roomsData[0])).to.be.an.instanceof(Booking);
+      expect(hotelOperation.createBooking(bookingsData[0])).to.be.an.instanceof(Booking);
     });
 
     it('should push new booking instance to the bookings record', () => {
@@ -66,6 +67,18 @@ describe('HotelOperation', () => {
       expect(hotelOperation.bookingsRecord.length).to.deep.equal(1);
     });
 
+    it('should have a method to create instances of user when passed an array', () => {
+      expect(hotelOperation.createUser(usersData[0])).to.be.an.instanceof(User);
+    });
+
+    it('should push new user instance to the users array', () => {
+      expect(hotelOperation.usersRecord.length).to.deep.equal(0);
+
+      hotelOperation.createUser(usersData[0]);
+
+      expect(hotelOperation.usersRecord.length).to.deep.equal(1);
+    });
+
     it('should have a method to create multiple instances of room when passed data', () => {
       let result = hotelOperation.createRoomsRecord(roomsData);
 
@@ -74,10 +87,43 @@ describe('HotelOperation', () => {
     });
 
     it('should have a method to create multiple instances of booking when passed data', () => {
+      hotelOperation.createUsersRecord(usersData);
       let result = hotelOperation.createBookingsRecord(roomsData);
 
       expect(hotelOperation.bookingsRecord[0]).to.be.an.instanceof(Booking);
       expect(hotelOperation.bookingsRecord[1]).to.be.an.instanceof(Booking);
+    });
+
+    it('should have a method to create multiple instances of user when passed data', () => {
+      let result = hotelOperation.createUsersRecord(usersData);
+
+      expect(hotelOperation.usersRecord[0]).to.be.an.instanceof(User);
+      expect(hotelOperation.usersRecord[1]).to.be.an.instanceof(User);
+    });
+
+    it('should have a method to find user name when given an id', () => {
+      hotelOperation.createUsersRecord(usersData);
+      let result = hotelOperation.findUserName(2);
+
+      expect(result).to.deep.equal('Theo Hernandez');
+    });
+    // TODO - write this test
+    it('should have a method to find user id when given a name', () => {
+      // hotelOperation.createUsersRecord(usersData);
+      // let result = hotelOperation.findUserName(2);
+      //
+      // expect(result).to.deep.equal('Theo Hernandez');
+    });
+
+    it('should filter bookings by user name', () => {
+      hotelOperation.createBookingsRecord(roomsData);
+      hotelOperation.createUsersRecord(usersData);
+
+      let result = hotelOperation.filterBookingsByName('Anson Aimes');
+
+      expect(result.length).to.deep.equal(2);
+      expect(result[0].userID).to.deep.equal(1);
+      expect(result[1].userID).to.deep.equal(1);
     });
 
   });
